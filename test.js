2dae2243aa7ec6208d71818f7f5f6543b1c658de
Simen Bekkhus <sbekkhus91@gmail.com>
2015-04-29 02:28:41 +0200

    init

---
+++ test.js
@@ -0,0 +1,53 @@
+/*jshint mocha:true*/
+
+'use strict';
+
+var reporter = require('./');
+
+var csslint = require('csslint').CSSLint;
+var chalk = require('chalk');
+
+var path = require('path');
+var assert = require('assert');
+
+describe('csslint-stylish', function () {
+  it('should report stuff', function () {
+    var res = csslint.verify('.class {\n  color: red !important;\n}\n');
+
+    var report = reporter.startFormat() + reporter.formatResults(res, path.resolve('style.css')) + reporter.endFormat();
+
+    report = chalk.stripColor(report);
+
+    var filename = report.split('\n')[ 0 ];
+
+    assert(filename === 'style.css', 'filename is correct');
+    assert(report.match(/line 2/), 'report contains text');
+    assert(report.match(/char 3/), 'report contains text');
+    assert(report.match(/Use of !important/), 'report contains text');
+    assert(report.match(/1 warning/), 'report contains text');
+  });
+
+  it('should report with full path', function () {
+    var res = csslint.verify('.class {\n  color: red !important;\n}\n');
+
+    var report = reporter.startFormat() + reporter.formatResults(res, path.resolve('style.css'),
+        { absoluteFilePathsForFormatters: true }) + reporter.endFormat();
+
+    report = chalk.stripColor(report);
+
+    var filename = report.split('\n')[ 0 ];
+
+    assert(filename === path.join(__dirname, 'style.css'), 'filename is correct');
+    assert(report.match(/char 3/), 'report contains text');
+    assert(report.match(/Use of !important/), 'report contains text');
+    assert(report.match(/1 warning/), 'report contains text');
+  });
+
+  it('should be able to be registered as formatter', function () {
+    assert(!csslint.hasFormat('stylish'), 'csslint should not be stylish');
+
+    csslint.addFormatter(reporter);
+
+    assert(csslint.hasFormat('stylish'), 'csslint should be stylish');
+  });
+});
