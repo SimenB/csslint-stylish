96e0f0bee29e957a1c97259a66dc042b62f05049
Simen Bekkhus <sbekkhus91@gmail.com>
2017-03-04 19:07:38 +0100

    Revert "Use snapshot assertions"
    This reverts commit 59b70c27c613f0ba54394f19da8738d40c60990b.
    
    Needs to wait for normalizing of path
    
    https://github.com/facebook/jest/pull/3007
    https://travis-ci.org/SimenB/csslint-stylish/builds/207717253

--- test.js
+++ test.js
@@ -10,19 +10,34 @@ import reporter from './stylish';
 test('should report stuff', () => {
   const res = CSSLint.verify('.class {\n  color: red !important\n}\n');
 
-  const report = reporter.startFormat() + reporter.formatResults(res, path.resolve('style.css')) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, path.resolve('style.css')) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  const filename = report.split('\n')[1];
+
+  expect(filename).toEqual('style.css', 'filename is correct');
+  expect(report).toMatch(/line 2/);
+  expect(report).toMatch(/char 3/);
+  expect(report).toMatch(/Use of !important/);
+  expect(report).toMatch(/1 warning/);
 });
 
 test('should report with full path', () => {
   const res = CSSLint.verify('.class {\n  color: red !important\n}\n');
 
-  const report = reporter.startFormat() +
+  let report = reporter.startFormat() +
     reporter.formatResults(res, path.resolve('style.css'), { absoluteFilePathsForFormatters: true }) +
     reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  const filename = report.split('\n')[1];
+
+  expect(filename).toEqual(path.join(__dirname, 'style.css'), 'filename is correct');
+  expect(report).toMatch(/char 3/);
+  expect(report).toMatch(/Use of !important/);
+  expect(report).toMatch(/1 warning/);
 });
 
 test('should be able to be registered as formatter', () => {
@@ -36,51 +51,67 @@ test('should be able to be registered as formatter', () => {
 test('should not report undefined output lines when no filename provided', () => {
   const res = CSSLint.verify('.class {\n  color: red !important\n}\n');
 
-  const report = reporter.startFormat() + reporter.formatResults(res) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).not.toMatch(/^undefined$/gm);
 });
 
 test('should report filename provided', () => {
   const res = CSSLint.verify('.class {\n  color: red !important\n}\n');
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() +
+  let report = reporter.startFormat() +
     reporter.formatResults(res, filename, { absoluteFilePathsForFormatters: true }) +
     reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).not.toMatch(/^undefined$/gm);
+  expect(report.split('\n')[1]).toEqual(filename, 'filename should be in output lines');
 });
 
 test('should report no violations if there are none', () => {
   const res = CSSLint.verify('.class {\n  color: red;\n}\n');
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report.trim()).toEqual('No violations');
 });
 
 test('should report errors', () => {
   const res = CSSLint.verify('.class {\n  color: red !important\n}\n', { important: 2 });
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).toMatch(/line 2/);
+  expect(report).toMatch(/char 3/);
+  expect(report).toMatch(/Use of !important/);
+  expect(report).toMatch(/1 error/);
 });
 
 test('should report multiple warnings', () => {
   const res = CSSLint.verify('.class {\n  color: red !important;\ntext-size: 12px !important;\n}\n', { important: 1 });
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).toMatch(/2 warnings/);
 });
 
 test('should report multiple errors', () => {
   const res = CSSLint.verify('.class {\n  color: red !important;\ntext-size: 12px !important;\n}\n', { important: 2 });
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).toMatch(/2 errors/);
 });
 
 test('should report rollups correctly', () => {
@@ -90,7 +121,12 @@ test('should report rollups correctly', () => {
     { floats: 2 }
   );
   const filename = path.resolve('filenamestyle.css');
-  const report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
+  let report = reporter.startFormat() + reporter.formatResults(res, filename) + reporter.endFormat();
 
-  expect(chalk.stripColor(report)).toMatchSnapshot();
+  report = chalk.stripColor(report);
+
+  expect(report).not.toMatch(/line /);
+  expect(report).not.toMatch(/char /);
+  expect(report).toMatch(/Too many floats \(11\), you're probably using them for layout. Consider using a grid system instead\./);
+  expect(report).toMatch(/1 warning/);
 });
